20a2c94a2c0fc3e1c7122b7d57911f8c6bb75d61
Corey Ford <corey@coreyford.name>
2014-04-08 20:14:14 +0200

    ListActivity with placeholder data

--- src/se/kth/ict/hotspots/MainActivity.java
+++ src/se/kth/ict/hotspots/MainActivity.java
@@ -1,15 +1,19 @@
 package se.kth.ict.hotspots;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                new String[] { "Home", "Work" });
+        setListAdapter(adapter);
     }
 
     @Override
