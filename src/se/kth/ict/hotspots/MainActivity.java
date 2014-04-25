805fc9dd10298339f0b51e464f3c31a90b3831df
Corey Ford <corey@coreyford.name>
2014-04-25 23:05:46 +0200

    Move AlarmManager work into an AlarmSetter
    The AlarmSetter is triggered on boot in addition to when MainActivity
    starts. There appears to be no simple way to "schedule the alarm for
    this PendingIntent only if not scheduled", but instead we get the
    behavior of "replace any existing scheduled alarm" which is good enough.
    
    Notable modifications along the way:
     - LocationPollerParameter timeout allowed to default to 2 minutes
     - Changed to AlarmManager.setInexactRepeating with
       INTERVAL_FIFTEEN_MINUTES, which gets us batched alarms on pre-KitKat

--- src/se/kth/ict/hotspots/MainActivity.java
+++ src/se/kth/ict/hotspots/MainActivity.java
@@ -1,37 +1,24 @@
 package se.kth.ict.hotspots;
 
-import java.io.IOException;
-
+import android.app.ListActivity;
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import se.kth.ict.hotspots.db.CityAdapter;
 import se.kth.ict.hotspots.db.DatabaseHelper;
 import se.kth.ict.hotspots.db.LocationAdapter;
-import android.app.AlarmManager;
-import android.app.ListActivity;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.location.Location;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.os.SystemClock;
-import android.util.Log;
-import android.view.Menu;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.Toast;
 
-import com.commonsware.cwac.locpoll.LocationPoller;
-import com.commonsware.cwac.locpoll.LocationPollerParameter;
+import java.io.IOException;
 
 public class MainActivity extends ListActivity {
 
-	private static final int PERIOD=15*60*1000; // 15 minutes
-	private PendingIntent pi=null;
-	private AlarmManager mgr=null;	
-    
-	@Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        sendBroadcast(new Intent(this, AlarmSetter.class));
 
         String city = null;
         try {
@@ -52,31 +39,7 @@ public class MainActivity extends ListActivity {
         ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
                 new String[] { "Home", city != null ? city : "Work" });
         setListAdapter(adapter);
-        
-    	mgr=(AlarmManager)getSystemService(ALARM_SERVICE);
-    	Intent i=new Intent(this, LocationPoller.class);
-
-    	Bundle bundle = new Bundle();
-    	LocationPollerParameter parameter = new LocationPollerParameter(bundle);
-    	parameter.setIntentToBroadcastOnCompletion(new Intent(this, LocationReceiver.class));
-    	// try GPS and fall back to NETWORK_PROVIDER
-    	parameter.setProviders(new String[] {LocationManager.GPS_PROVIDER, LocationManager.NETWORK_PROVIDER});
-    	parameter.setTimeout(30000);
-    	i.putExtras(bundle);
-
-
-    	pi=PendingIntent.getBroadcast(this, 0, i, 0);
-    	mgr.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-    			SystemClock.elapsedRealtime(),
-    			PERIOD,
-    			pi);
-
-    	Toast
-    	.makeText(this,
-    	"Location polling every 15 minutes begun",
-    	Toast.LENGTH_LONG)
-    	.show();
-    	  }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
