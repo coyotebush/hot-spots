6ccb2ea2a5faa6494341d63c7e3eac6c0726ca8d
Corey Ford <corey@coreyford.name>
2014-04-25 23:25:53 +0200

    Swap lat/lon in getTopFavorites query
    It seems slightly clearer to keep the order of the columns the same as
    the constructor arguments.

--- src/se/kth/ict/hotspots/db/FavoriteAdapter.java
+++ src/se/kth/ict/hotspots/db/FavoriteAdapter.java
@@ -17,14 +17,14 @@ public class FavoriteAdapter {
     }
 
     public List<Favorite> getTopFavorites() throws jsqlite.Exception {
-        Stmt stmt = db.prepare("SELECT name, X(Centroid(geom)), Y(Centroid(geom)) "
+        Stmt stmt = db.prepare("SELECT name, Y(Centroid(geom)), X(Centroid(geom)) "
                 + "FROM favorite ORDER BY weight DESC");
         List<Favorite> result = new ArrayList<Favorite>();
         while (stmt.step()) {
             result.add(new Favorite(
                     stmt.column_string(0),
-                    stmt.column_double(2),
-                    stmt.column_double(1)
+                    stmt.column_double(1),
+                    stmt.column_double(2)
             ));
         }
         return result;
